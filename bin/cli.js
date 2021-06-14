968edcfedaccfebc584b7d0bcb3f3e12d802015f
mehan <mehan.alavi.88.majd@gmail.com>
2021-06-14 23:11:18 +0430

    Fixed bug

--- bin/cli.js
+++ bin/cli.js
@@ -1,9 +1,9 @@
 #!/usr/bin/env node
-let exec = require("../exec")
+let exec = require("../exec").exec
 let args = require("process").argv;
 let chalk = require("chalk")
 args = args.slice(2);
 if (args.length !== 1) {
   throw Error(chalk.red("please only put name after make-applecake-app"))
 }
-exec.exec("app")
+exec(args[0])
