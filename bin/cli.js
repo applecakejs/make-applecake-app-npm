13083409299af1e52f84b73432b9e0e5b4fc2f12
mehan <mehan.alavi.88.majd@gmail.com>
2021-06-14 22:50:29 +0430

    added chalk

--- bin/cli.js
+++ bin/cli.js
@@ -1,8 +1,9 @@
 #!/usr/bin/env node
 let exec = require("../exec")
 let args = require("process").argv;
+let chalk = require("chalk")
 args = args.slice(2);
 if (args.length !== 1) {
-  throw Error("please only put name after make-applecake-app")
+  throw Error(chalk.red("please only put name after make-applecake-app"))
 }
 exec.exec("app")
